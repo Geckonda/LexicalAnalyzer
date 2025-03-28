7f93edcf61e632a9ed1d4e72de8ec540131e71be
Geckonda <Aquariusstd@yandex.ru>
2025-03-28 17:48:39 +0400

    пофиксил проблемы лексического анализатора

--- Lab1/LexicalAnalyzer.cs
+++ Lab1/LexicalAnalyzer.cs
@@ -95,8 +95,8 @@ namespace Lab1
             while (_identifierState != IdentifierStates.Fin)
             {
                 stateHandlers[_identifierState]?.Invoke();
-                token.Type = TokenKind.Identifier;
             }
+            token.Type = TokenKind.Identifier;
         }
         private void HandleStateA()
         {
@@ -245,6 +245,8 @@ namespace Lab1
                                 state = DigitStates.F;
                                 HandleSymbol();
                             }
+                            else if( _tr.CurSym == '1')
+                                LexicalError("Ожидалось '0' или конец"); // Обнаружена ошибка в тексте.
                             else
                                 state = DigitStates.Fin;
                             break;
